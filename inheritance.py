cb2d50a4415a14820a747d517e9255fa40b0c53b
sabram <66972158+sabram20@users.noreply.github.com>
2020-08-31 18:35:26 -0500

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,12 @@
+class Acura:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+class RDX(Acura):
+    def __init__(self, selfParking, make, model, year):
+        Acura.__init__(self, make, model, year)
+        self.selfParking = selfParking
+
+RDX = RDX(True, "Acura", "AWD", "2020")
+print(RDX.model)
